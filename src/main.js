ff7db5e8fdb5f28fd07c77b531d60173034c29ce
Taewoong Moon <moondingie5@gmail.com>
2018-09-17 18:27:35 +0900

    firebase 초기설정

--- src/main.js
+++ src/main.js
@@ -3,8 +3,50 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
+import { config } from './config'
+require("firebase/firestore");
+
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  config
+};
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
+db.collection("users").add({
+  first: "Ada",
+  last: "Lovelace",
+  born: 1815
+}).then(function(docRef) {
+  console.log("Document written with ID: ", docRef.id);
+}).catch(function(error) {
+  console.error("Error adding document: ", error);
+});
+
+db.collection("users").add({
+  first: "Alan",
+  middle: "Mathison",
+  last: "Turing",
+  born: 1912
+}).then(function(docRef) {
+  console.log("Document written with ID: ", docRef.id);
+}).catch(function(error) {
+  console.error("Error adding document: ", error);
+});
+
+db.collection("users").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+  });
+});
+
+
+
+
+
 
 /* eslint-disable no-new */
 new Vue({
